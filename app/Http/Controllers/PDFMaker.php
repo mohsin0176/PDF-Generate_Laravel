bb0c3b89138ecdbcc52e86dc89ddb8da57ca3b5a
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-28 07:51:29 -0700

    Pdf generator environment setup complete

---
+++ app/Http/Controllers/PDFMaker.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PDFMaker extends Controller
+{
+    public function make()
+    {
+    	// grab some html
+    	// create pdf class
+    	// send html to the class
+    	// then respond with pdf back to the browser or
+    	// send to the file system
+    }
+}
